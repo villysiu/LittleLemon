4855d8685fc44a06810bfe2ebb6db293f723d1ee
Villy Siu <villysiu@gmail.com>
2023-12-26 19:38:06 -0800

    djoser

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'reservation',
-    'restaurant'
+    'restaurant',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,16 @@ STATIC_URL = 'restaurant/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+
+    # Add code to assign default authentication classes
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ]
+    
+}
+
+DJOSER = {
+  "USER_TO_FIELD": "username"
+}
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,15 +1,12 @@
 #define URL route for index() view
 from django.urls import path, include
 from . import views
-from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 
-    path('booking/', include(router.urls)),
+    
 ]
