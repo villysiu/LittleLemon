0eff0fb1077c87bd3f27878c64dc01ff55fd390a
Villy Siu <villysiu@gmail.com>
2023-12-26 19:37:51 -0800

    setup djoser

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -1,9 +1,17 @@
 #update URLConf by including URL patterns of restaurant app
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant import views
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('restaurant/', include('restaurant.urls')),
    # path('restaurant/menu/',include('restaurant.urls')),
+   path('booking/', include(router.urls)),
+   path('auth/', include('djoser.urls')),
+   path('auth/', include('djoser.urls.authtoken'))
 ]
